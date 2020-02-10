3e886cddea049670dfa05884cd9e7cdfaf1e91d3
mike-briggs <michael.briggs@queensu.ca>
2020-02-09 20:50:21 -0500

    change ref

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template, send_from_directory
 import os
 
 
-app = Flask(__name__,static_folder="./client/build/static", template_folder="./client/build")
+app = Flask(__name__,static_folder="./build/static", template_folder="./build")
 
 @app.route("/")
 def hello():
